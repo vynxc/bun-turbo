05a2b08cf34337465332dcad3ad6dabdf1a2db59
chaycee <svynxc@gmail.com>
2023-11-03 15:02:04 -0400

    added decorators

---
+++ types/decorators/get.ts
@@ -0,0 +1,18 @@
+﻿import { Turbo } from "../../router";
+import { RouteHandler } from "../bun-route";
+
+
+export function Get<Route extends string>(routeString: Route,server:Turbo) {
+    return function (
+        target: Object,
+        propertyKey: string,
+        descriptor: TypedPropertyDescriptor<RouteHandler<Route,Response>>
+    ) {
+        const method = descriptor.value!;
+        server.get(routeString, method);
+        console.log('done')
+    };
+}
+
+
+
